c641e0e8ed1c130ac5a449c48e6e6e2bbb873aa1
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-03 08:34:23 +0530

    [Bindhu]Add: Ability to analyse and respond

--- MoodAnalyserProject1/MoodAnalyserProblem/Program.cs
+++ MoodAnalyserProject1/MoodAnalyserProblem/Program.cs
@@ -1,4 +1,7 @@
-﻿using MoodAnalyserProblem;
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using MoodAnalyserProblem;
 
 string message1 = "I am in Sad mood";
 MoodAnalyser mood1 = new MoodAnalyser(message1);
